0838a0028b63fd892e06eee4ca94d8592c58a1a5
Travis Nicholson <110001213+Travisnicholson90@users.noreply.github.com>
2023-02-07 14:20:02 +1100

    code

---
+++ script.js
@@ -0,0 +1,9 @@
+// console.log('input output')
+
+
+// const input = document.querySelector('.input')
+// const output = document.querySelector('.output')
+
+
+// output.value.textContent = this.input.value
+
