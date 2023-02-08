77e58b95722a20410611cd3e95d4279dca5324ee
Travis Nicholson <110001213+Travisnicholson90@users.noreply.github.com>
2023-02-08 12:53:26 +1100

    working-code

--- script.js
+++ script.js
@@ -1,9 +1,59 @@
-// console.log('input output')
+
+const form = document.querySelector('.form')
+// * name duplicate form to card
+const nameInput = document.querySelector('.name-input')
+const nameOutput = document.querySelector('.output-name')
+
+// * number duplicate form to card
+const numberInput = document.querySelector('.number-input')
+const numberOutput = document.querySelector('.output-number')
 
 
-// const input = document.querySelector('.input')
-// const output = document.querySelector('.output')
+// * expiry month duplicate form to card
+const mmInput = document.querySelector('.exp-mm')
+const mmOutput = document.querySelector('.output-mm')
+
+// * expiry year duplicate form to card
+const yyInput = document.querySelector('.exp-yy')
+const yyOutput = document.querySelector('.output-yy')
+
+// * cvc duplicate form to card
+const cvcInput = document.querySelector('.cvc')
+const cvcOutput = document.querySelector('.cvc-output-number')
 
 
-// output.value.textContent = this.input.value
 
+
+function names() {
+    
+    nameInput.addEventListener('input', function() {
+        nameOutput.value = this.value
+        
+    })
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    names()
+
+})    
+
+
+
+numberInput.addEventListener('input', function() {
+    numberOutput.value = this.value
+
+})
+
+mmInput.addEventListener('input', function() {
+    mmOutput.value = this.value
+})
+
+yyInput.addEventListener('input', function() {
+    yyOutput.value = this.value
+})
+
+cvcInput.addEventListener('input', function() {
+    cvcOutput.value = this.value
+})
